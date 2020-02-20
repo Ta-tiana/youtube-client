1c88798ca594068727b7e78500060be2e8d99215
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-02-20 15:51:48 +0300

    feat: add providers file

---
+++ my-angular-app/src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataService } from "../service/data.service";
+import { ModuleWithProviders } from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        DataService,
+      ]
+    };
+  }
+}
