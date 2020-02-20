30f36a405d98da52c61c5cb94aaa377c460ac3eb
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-02-20 15:44:11 +0300

    feat: update header component

--- my-angular-app/src/app/components/header/header.component.ts
+++ my-angular-app/src/app/components/header/header.component.ts
@@ -2,7 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+
+  template:
+  `<header class="header">
+
+    <!-- logo -->
+    <div class="logo">
+      <img class="logo-item" src="../../../favicon.ico" alt="logo">
+      <span class="youtube-client">&nbsp;Youtube-client</span>
+    </div>
+
+    <!--navigation-->
+    <nav class ="navbar">
+      <app-search></app-search>
+    </nav>
+
+  </header>`,
+
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
