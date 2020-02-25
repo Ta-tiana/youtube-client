12058cca378c8c66846d0d7ee0616d727bb8d506
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-02-26 00:56:57 +0300

    feat: add youtube directive

---
+++ my-angular-app/src/app/youtube/directives/youtube.directive.spec.ts
@@ -0,0 +1,8 @@
+import { YoutubeDirective } from './youtube.directive';
+
+describe('SharedDirective', () => {
+  it('should create an instance', () => {
+    const directive = new YoutubeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
