7bfb4d13ad9e3127cd8238693c0145e8fe6d88b8
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-02-27 08:57:24 +0300

    feat: update login page

--- my-angular-app/src/app/auth/pages/login/login.component.ts
+++ my-angular-app/src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router, public authService: AuthService) { }
 
-  ngOnInit(): void {
+  public goToRegistration(): void {
+    this.router.navigateByUrl(`auth/registration`).then(r => console.log(r)) ;
   }
 
+  public logIn(form: any): void {
+    this.authService.setLogInfoValue(form.__ngContext__[28].value, form.__ngContext__[33].value);
+    if (this.authService.isInfoEqual()) {
+      alert(`Добро пожаловать, ${this.authService.regFName}!`);
+      this.authService.setToken();
+      this.router.navigateByUrl(``).then(r => console.log(r)) ;
+    } else {
+      alert(` ${this.authService.regPassword ? 'Неверный пароль! псс..пароль: ' +
+        this.authService.regPassword : 'Пользователь не найден!' } ` );
+    }
+  }
+
+  public ngOnInit(): void {
+  }
 }
