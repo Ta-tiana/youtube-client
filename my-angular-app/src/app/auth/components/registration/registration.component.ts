37759bd1029490afe7e3d8cbd971c433c299fec0
Tatsiana Kharlamava <ustinya88@gmail.com>
2020-03-09 23:52:16 +0300

    feat: update registration and login components

--- my-angular-app/src/app/auth/components/registration/registration.component.ts
+++ my-angular-app/src/app/auth/components/registration/registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {AuthService} from '../../services/auth.service';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-registration',
@@ -9,16 +9,21 @@ import {AuthService} from '../../services/auth.service';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor( private router: Router, public authService: AuthService ) { }
+  constructor( private router: Router, public loginService: LoginService) { }
 
   public goToLogin(): void {
     this.router.navigateByUrl(`auth/login`).then(r => console.log(r) ) ;
   }
 
   public SignIn(form: any): void {
-    this.authService.setRegInfoValue(form.__ngContext__[28].value, form.__ngContext__[33].value,
-    form.__ngContext__[38].value, form.__ngContext__[43].value);
-    if (this.authService.isRegFromFilled()) {
+    this.loginService.setRegInfoValue
+    (
+      form.__ngContext__[28].value,
+      form.__ngContext__[33].value,
+      form.__ngContext__[38].value,
+      form.__ngContext__[43].value
+    );
+    if (this.loginService.isRegFromFilled()) {
       alert('Вы зарегистрированы! Введите свой логин и пароль!');
       this.router.navigateByUrl(`auth/login`).then(r => console.log(r)) ;
     }
--- my-angular-app/src/app/auth/pages/login/login.component.ts
+++ my-angular-app/src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {AuthService} from '../../services/auth.service';
+import { AuthService } from '../../services/auth.service';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,21 +10,25 @@ import {AuthService} from '../../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor( private router: Router, public authService: AuthService) { }
+    constructor (
+      private router: Router,
+      public authService: AuthService,
+      public loginService: LoginService
+    ) { }
 
   public goToRegistration(): void {
     this.router.navigateByUrl(`auth/registration`).then(r => console.log(r)) ;
   }
 
   public logIn(form: any): void {
-    this.authService.setLogInfoValue(form.__ngContext__[28].value, form.__ngContext__[33].value);
-    if (this.authService.isInfoEqual()) {
-      alert(`Добро пожаловать, ${this.authService.regFName}!`);
+    this.loginService.setLogInfoValue(form.__ngContext__[28].value, form.__ngContext__[33].value);
+    if (this.loginService.isInfoEqual()) {
+      alert(`Добро пожаловать, ${this.loginService.regFName}!`);
       this.authService.setToken();
       this.router.navigateByUrl(``).then(r => console.log(r)) ;
     } else {
-      alert(` ${this.authService.regPassword ? 'Неверный пароль! псс..пароль: ' +
-        this.authService.regPassword : 'Пользователь не найден!' } ` );
+      alert(` ${this.loginService.regPassword ? 'Неверный пароль! псс..пароль: ' +
+        this.loginService.regPassword : 'Пользователь не найден!' } ` );
     }
   }
 
